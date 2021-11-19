ed833aebcd4f6708e46a7ae564f9f6d727125c97
Nico <ncollie42@gmail.com>
2021-11-18 19:13:58 -0800

    [Menu] Add temporary title

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -3,17 +3,10 @@ extends Control
 signal quit
 
 func _input(event):
+	raise()
 	if Input.is_action_just_pressed("pause"):
-		if $Sound_menu.visible:
-			$Sound_menu.hide()
-			return
 		get_tree().paused = !get_tree().paused
 		visible = get_tree().paused
-		$Sound_menu.hide()
-
-func _on_volume_pressed():
-	$Sound_menu.visible = !$Sound_menu.visible
-
 
 func _on_quit_pressed():
 	emit_signal("quit")
