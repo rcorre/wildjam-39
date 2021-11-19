6eb19b03a895bd267f4f9d18fb538d330e230ffb
Nico <ncollie42@gmail.com>
2021-11-19 13:29:34 -0800

    [Chest] Food / Bomb queue free after animation

--- scripts/Chest.gd
+++ scripts/Chest.gd
@@ -6,20 +6,18 @@ onready var anim: AnimationPlayer = $AnimationPlayer
 
 export(global.Item) var item
 
-var interacted := false
-
 func _enter_tree():
 	add_to_group("chest")
 
 func _on_interactable_interact(player_interactable):
 	print("Player was given a ", global.Item.keys()[item])
-	if item == global.Item.FOOD and not interacted:
-		interacted = true
+	if item == global.Item.FOOD:
 		player_interactable.emit_signal("food_pick_up", item)
+		$AnimationPlayer.connect("animation_finished", self, "_on_animation_finished")
 		emit_signal("opened")
-	elif item == global.Item.BOMB and not interacted:
-		interacted = true
+	elif item == global.Item.BOMB:
 		player_interactable.emit_signal("bomb_pick_up")
+		$AnimationPlayer.connect("animation_finished", self, "_on_animation_finished")
 		emit_signal("opened")
 	else:
 		var current_player_weapon = player_interactable.current_weapon
@@ -29,7 +27,10 @@ func _on_interactable_interact(player_interactable):
 
 	if is_in_group("chest"):
 		remove_from_group("chest")
-#	$sfx.play()
+	$sfx.play()
 	anim.play("Open")
 	yield(get_tree().create_timer(2), "timeout")
 	anim.play_backwards("Open")
+	
+func _on_animation_finished(animation):
+	queue_free()
