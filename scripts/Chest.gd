301205e4ae1763fd900a1dd78e29b3df650ab65b
Ryan Roden-Corrent <ryan@rcorre.net>
2021-11-19 08:08:25 -0500

    Animate chest explosion.

--- scripts/Chest.gd
+++ scripts/Chest.gd
@@ -1,24 +1,30 @@
 extends StaticBody
 
+signal opened()
+
 onready var anim: AnimationPlayer = $AnimationPlayer
 
 export(global.Item) var item
+
+var interacted := false
 
 func _enter_tree():
 	add_to_group("chest")
 
 func _on_interactable_interact(player_interactable):
 	print("Player was given a ", global.Item.keys()[item])
-	if item == global.Item.FOOD:
+	if item == global.Item.FOOD and not interacted:
+		interacted = true
 		player_interactable.emit_signal("food_pick_up", item)
-		queue_free()
-	elif item == global.Item.BOMB:
+		emit_signal("opened")
+	elif item == global.Item.BOMB and not interacted:
+		interacted = true
 		player_interactable.emit_signal("bomb_pick_up")
-		#TODO: Add Small particle explosion
-		queue_free()
+		emit_signal("opened")
 	else:
 		var current_player_weapon = player_interactable.current_weapon
 		player_interactable.emit_signal("weapon_pick_up", item)
 		item = current_player_weapon
+		emit_signal("opened")
 
 	anim.play("Open")
