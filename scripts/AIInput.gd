45049642b96053d6b42b63ddc642bdf1b4ae6d24
Ryan Roden-Corrent <ryan@rcorre.net>
2021-11-19 08:31:02 -0500

    Add pathfinding to AI.

--- scripts/AIInput.gd
+++ scripts/AIInput.gd
@@ -5,9 +5,31 @@ signal mobile_look_towards(look_vect)
 signal move_towards(move_dir)
 signal attack()
 
-var interacted := {}
+onready var nav: Navigation = get_tree().get_nodes_in_group("nav")[0]
+onready var exit: Vector3 = get_tree().get_nodes_in_group("exit")[0].global_transform.origin
 
-func _physics_process(delta):
+var interacted := {}
+var path: PoolVector3Array
+var path_debug: ImmediateGeometry
+
+func _ready():
+	if OS.is_debug_build():
+		path_debug = ImmediateGeometry.new()
+		add_child(path_debug)
+
+func _process(delta: float):
+	if not path_debug:
+		return
+
+	path_debug.clear()
+	path_debug.begin(Mesh.PRIMITIVE_LINE_STRIP)
+	path_debug.set_color(Color.red)
+	for p in path:
+		p.y += 1  # show it a bit above the map
+		path_debug.add_vertex(p)
+	path_debug.end()
+
+func _physics_process(delta: float):
 	var pos := global_transform.origin
 
 	var enemy := nearest_visible("enemy")
@@ -19,6 +41,7 @@ func _physics_process(delta):
 		else:
 			emit_signal("move_towards", Vector3.ZERO)
 			emit_signal("attack")
+		return
 
 	var chest := nearest_visible("chest")
 	if chest:
@@ -30,9 +53,18 @@ func _physics_process(delta):
 			emit_signal("move_towards", Vector3.ZERO)
 			Input.action_press("interact")
 			interacted[chest] = true
+		return
 
-	#if Input.is_action_just_pressed("attack"):
-	#	emit_signal("attack")
+	if not path:
+		print("Pathfinding update")
+		path = nav.get_simple_path(pos, exit)
+
+	var target := path[0]
+	emit_signal("look_towards", target)
+	if pos.distance_to(target) > 0.8:
+		emit_signal("move_towards", pos.direction_to(target))
+	else:
+		path.remove(0)
 
 func nearest_visible(group: String) -> Spatial:
 	var nearest: Spatial = null
